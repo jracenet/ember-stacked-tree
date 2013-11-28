e1aeeca8a274204d99d9338f0074d9310cc77d59
jracenet <joan.racenet@smartesting.com>
2013-11-28 11:44:07 +0100

    Add color gradient on nodes header

--- js/app.js
+++ js/app.js
@@ -38,41 +38,41 @@ App.IndexRoute = Ember.Route.extend({
 
 App.NodeTreeView = Ember.View.extend({
 	tagName: 'div',
+  classNames: ['node'],
 
 	didInsertElement: function () {
-		var color = this.get('parentView').$('.node-header').css('background-color');
-		this.$('.node-header').css('opacity', this.shadeColor(color, -20));
-		console.log(color);
+    if(this.get('parentView.viewName') === 'nodeTree') {
+  		var color = this.get('parentView').$('.node-header:first').css('background-color'),
+          newColor = this.shadeColor(color, 10);
+
+  		this.$('.node-header:first').css('background-color', newColor);
+    }
 	},
 
 	shadeColor: function (color, percent) {
-		console.log('COLOR', color, "TYPE", typeof(color));
-    // var R = parseInt(color.substring(1,3),16);
-    // var G = parseInt(color.substring(3,5),16);
-    // var B = parseInt(color.substring(5,7),16);
+    var R, G, B, colorComponents;
 
-    // R = parseInt(R * (100 + percent) / 100);
-    // G = parseInt(G * (100 + percent) / 100);
-    // B = parseInt(B * (100 + percent) / 100);
+    colorComponents = color.match(/\d+/g)
 
-    // R = (R<255)?R:255;
-    // G = (G<255)?G:255;
-    // B = (B<255)?B:255;
+    R = parseInt(colorComponents[0], 10);
+    G = parseInt(colorComponents[1], 10);
+    B = parseInt(colorComponents[2], 10);
 
-    // var RR = ((R.toString(16).length==1)?"0"+R.toString(16):R.toString(16));
-    // var GG = ((G.toString(16).length==1)?"0"+G.toString(16):G.toString(16));
-    // var BB = ((B.toString(16).length==1)?"0"+B.toString(16):B.toString(16));
+    R = parseInt(R * (100 + percent) / 100);
+    G = parseInt(G * (100 + percent) / 100);
+    B = parseInt(B * (100 + percent) / 100);
 
-    // return "#"+RR+GG+BB;
+    R = (R < 255)? R : 255;
+    G = (G < 255)? G : 255;
+    B = (B < 255)? B : 255;
+
+    return "rgb(%@, %@, %@)".fmt(R, G, B);
 	}
 
 });
 
 App.NodeTreeController = Ember.ObjectController.extend({
 	collapsed: false,
-
-	init: function() {
-	},
 
 	hasChilds: function () {
 		return !Ember.isEmpty(this.get('content.childs'));
